0933eb1ae126ceea6c1ffce23e6d94323375022c
Shiyun <serene.hoshiyun@gmail.com>
2024-09-30 19:17:56 +0800

    Completed

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 12:51:36 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/09/29 22:10:43 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2024/09/30 19:16:04 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,80 +14,69 @@
 #include <sys/types.h>
 #include <signal.h>
 
-// void    confirmation(int signum)
-// {
-//     static int  msg_confirmed;
-
-//     if (!msg_confirmed)
-//     {
-//         if (signum == SIGUSR1)
-//         {
-//             ft_printf("Message has been received successfully.\n");
-//             msg_confirmed = 1;
-//         }
-//     }
-// }
-
-void    send_char_to_bit(pid_t input_pid, char character)
+void	send_char_to_bit(pid_t input_pid, char character)
 {
-    int bit;
-    
-    bit = 7;
-    while (bit >= 0)
-    {
-        if ((character & (1 << bit))  == 0)
-            kill(input_pid, SIGUSR1);
-        else
-            kill(input_pid, SIGUSR2);        
-        // usleep(1000);
-        bit--;
-    }
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		if ((character & (1 << bit)) == 0)
+			kill(input_pid, SIGUSR1);
+		else
+			kill(input_pid, SIGUSR2);
+		bit--;
+		usleep(450);
+	}
 }
 
-void signal_handler(int signum)
+void	signal_handler(int signum)
 {
-    if (signum == SIGUSR2)
-        ft_printf("\nMessage received successfully by the server.\n");
+	if (signum == SIGUSR2)
+	{
+		ft_printf("Acknowledgement received by client from server\n");
+		exit(EXIT_SUCCESS);
+	}
 }
 
+void	send_msg_to_server(pid_t input_pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		send_char_to_bit(input_pid, str[i]);
+		i++;
+		usleep(450);
+	}
+	send_char_to_bit(input_pid, '\0');
+}
 
 /**
  * @brief: Sends str passed as param to server to print
  * @param: server_pid - PID number from server
  * @param: string_to send
  */
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int     input_pid;
-    int     i;
-    struct  sigaction sa;
+	int					input_pid;
+	struct sigaction	sa;
 
-    if (argc == 3)
-    {
-        input_pid = ft_atoi(argv[1]);
-        if (input_pid == -1 || input_pid > 2261779)
-            exit(EXIT_FAILURE);
-        
-        sa.sa_handler = &signal_handler;
-        sigaction(SIGUSR1, &sa, NULL);
-        sigaction(SIGUSR2, &sa, NULL);
-        
-        i = 0;
-        while(argv[2][i] != '\0')
-        {
-            send_char_to_bit(input_pid, argv[2][i]);
-            i++;
-            pause();
-        }
-        send_char_to_bit(input_pid, '\0');
-        pause();
-        // confirmation(signum);
-        // usleep(1000);
-    }
-    else
-    {
-        ft_printf("Make sure that input argument(s) are as such: \n");
-        ft_printf("./client [1: Server PID], [2: String to send]\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc == 3)
+	{
+		input_pid = ft_atoi(argv[1]);
+		if (input_pid == -1 || input_pid > 2261779)
+			exit(EXIT_FAILURE);
+		sa.sa_handler = &signal_handler;
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		send_msg_to_server(input_pid, argv[2]);
+		pause();
+	}
+	else
+	{
+		ft_printf("./client [1: Server PID], [2: String to send]\n");
+		exit(EXIT_FAILURE);
+	}
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 12:48:25 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/09/29 22:16:51 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:48:25 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,74 +14,65 @@
 #include <sys/types.h>
 #include <signal.h>
 
-pid_t client_pid;
+pid_t	g_client_pid;
 
-void bit_to_char(int *binary_form, siginfo_t *info)
+void	bit_to_char(int *binary_form, siginfo_t *info)
 {
-    char character;
-    int i;
+	char	character;
+	int		i;
 
-    i = 0;
-    character = 0;
-    while (i < 8)
-    {
-        character = character << 1;
-        character = character | binary_form[i];
-        i++;
-    }
-    ft_printf("%c", character);
-    if (character == '\0')
-    {
-        ft_printf("\n");
-        kill(info->si_pid, SIGUSR2);
-    }
+	i = 0;
+	character = 0;
+	while (i < 8)
+	{
+		character = character << 1;
+		character = character | binary_form[i];
+		i++;
+	}
+	ft_printf("%c", character);
+	if (character == '\0')
+	{
+		ft_printf("\n");
+		kill(info->si_pid, SIGUSR2);
+	}
 }
 
-void signal_handler(int signum)
+void	signal_handler(int signum, siginfo_t *info, void *u_context)
 {
-    static int binary_form[8];
-    static int bit_index;
+	static int	binary_form[8];
+	static int	bit_index;
 
-    if (signum == SIGUSR1)
-    {
-        binary_form[bit_index] = 0;
-        bit_index++;
-    }
-    else if (signum == SIGUSR2)
-    {    
-        binary_form[bit_index] = 1;
-        bit_index++;
-    }
-    
-    if (bit_index == 8)
-    {
-        bit_to_char(binary_form, info);
-        bit_index = 0;
-        kill(info->si_pid, SIGUSR1);
-    }
+	u_context += 1;
+	u_context -= 1;
+	if (signum == SIGUSR1)
+	{
+		binary_form[bit_index] = 0;
+		bit_index++;
+	}
+	else if (signum == SIGUSR2)
+	{
+		binary_form[bit_index] = 1;
+		bit_index++;
+	}
+	if (bit_index == 8)
+	{
+		bit_to_char(binary_form, info);
+		bit_index = 0;
+	}
 }
 
-
-int main()
+int	main(void)
 {
-    pid_t   server_pid;
-    //Declare sigaction structure
-    struct  sigaction sa;
-    
-    server_pid = getpid();
-    ft_printf("Server started. Printing process id(pid): %i\n", server_pid);
-    
-    //set signal handler as default action
-    sa.sa_handler = &signal_handler;
+	pid_t				server_pid;
+	struct sigaction	sa;
 
-    //initialise signal handler to handle different signals 
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);         
-
-    while(1)
-    {
-        pause();
-    }
-    
-    return (1);
+	server_pid = getpid();
+	ft_printf("Server started. Printing process id(pid): %i\n", server_pid);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (1);
 }
